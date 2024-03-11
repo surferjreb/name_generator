58f11358e42a418fd91af1cb4b8796da7276b547
[surferjreb] <[surferjreb@live.com]>
2024-03-10 21:22:01 -0400

    started manger

--- manager.py
+++ manager.py
@@ -1,4 +1,3 @@
-from name import Name
 from names import Names
 from name_gen import NameGen
 
@@ -6,9 +5,25 @@ from name_gen import NameGen
 class Manager:
 
     def __init__(self):
-        self.name = Name()
         self.names = Names()
-        self.ng = NameGen()
+        self.ng = NameGen(names=self.names)
+        self.ran = False
+
+    def run_name_gen(self):
+        try:
+            self.ng.generate_names()
+            self.ran = True
+        except Exception:
+            raise Exception('Name generator failed to run')
+
+    def set_name_inputs(self, nameType, nameStr)
+        pass
+
+    def print_names_list(self):
+        pass
+
+    def output_names_list(self):
+        pass
 
     def __repr__(self):
         return 'Manager'
